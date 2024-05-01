e6bc45d6840d3997f46283f7281a28dfb0ca91dc
runweiting <whatisfuntime@gmail.com>
2024-05-01 12:07:00 +0800

    feat: 🎸 修改 app.js middleware 執行錯誤處理

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const cors = require("cors");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-const postsRouter = require("./routes/posts");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
 var app = express();
-require("./connections");
+require('./connections');
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/posts", postsRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 // 在所有路由之後，處理 404
 app.use((req, res, next) => {
   res.status(404).send("Sorry, the page you're looking for doesn't exist.");
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('程式有些問題，請稍後嘗試');
 });
 
 module.exports = app;
