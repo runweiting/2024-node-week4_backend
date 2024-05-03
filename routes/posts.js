5ac6f48ac92bdf543a56694a837ef5d719e0b0ec
runweiting <whatisfuntime@gmail.com>
2024-05-03 18:15:05 +0800

    test: 💍 routes/posts 測試 contentRequired

--- routes/posts.js
+++ routes/posts.js
@@ -2,19 +2,9 @@ var express = require('express');
 var router = express.Router();
 const PostsController = require('../controllers/posts');
 const handleErrorAsync = require('../statusHandle/handleErrorAsync');
-const appError = require('../statusHandle/appError');
-
-// 檢查必填
-const contentRequired = (req, res, next) => {
-  if (req.body.content !== undefined) {
-    next();
-  } else {
-    next(appError(400, '內容為必填'));
-  }
-};
 
 router.get('/', PostsController.getPosts);
-router.post('/', contentRequired, handleErrorAsync(PostsController.createPost));
+router.post('/', handleErrorAsync(PostsController.createPost));
 router.patch('/:id', handleErrorAsync(PostsController.updatePost));
 router.delete('/', PostsController.deleteAllPost);
 router.delete('/:id', handleErrorAsync(PostsController.deletePost));
--- statusHandle/appError.js
+++ statusHandle/appError.js
@@ -1,4 +1,4 @@
-const appError = (httpStatus, errMessage) => {
+const appError = (httpStatus, errMessage, next) => {
   const error = new Error(errMessage);
   error.statusCode = httpStatus;
   // 自定屬性讓全域錯誤捕捉判斷是否為預期錯誤
