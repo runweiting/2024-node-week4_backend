0f97691b359bb5ec6aaf9d8853fd4d204b1a6a83
runweiting <whatisfuntime@gmail.com>
2024-05-03 16:44:50 +0800

    feat: 🎸 appError 帶入 next 參數

--- statusHandle/appError.js
+++ statusHandle/appError.js
@@ -1,9 +1,9 @@
-const appError = (httpStatus, errMessage) => {
+const appError = (httpStatus, errMessage, next) => {
   const error = new Error(errMessage);
   error.statusCode = httpStatus;
   // 自定屬性讓全域錯誤捕捉判斷是否為預期錯誤
   error.isOperational = true;
-  return error;
+  return next(error);
 };
 
 module.exports = appError;
