ae5a3b52a0c7d8c33cf80be302611eddf13905a8
runweiting <whatisfuntime@gmail.com>
2024-05-13 09:01:22 +0800

    feat: 🎸 app 修正 dotenv 匯入方式

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const {
   handleDevError,
   handleProError,
