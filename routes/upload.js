34955668ed21d46aac9ff103d0decf9c93c719a2
runweiting <whatisfuntime@gmail.com>
2024-05-20 15:06:25 +0800

    feat: 🎸 routes/upload blob.getSignedUrl 一起更新 User

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
+  handleResponse,
   handleErrorAsync,
   handleAppError,
 } = require('../statusHandle/handleResponses');
@@ -10,6 +11,7 @@ const isImage = require('../tools/isImage');
 const firebaseAdmin = require('../tools/firebase');
 // bucket 雲存儲桶是 Firebase Admin SDK 中對應於一個存儲桶的對象
 const bucket = firebaseAdmin.storage().bucket();
+const User = require('../models/usersModel');
 
 router.post(
   '/file',
@@ -25,21 +27,24 @@ router.post(
     const blob = bucket.file(
       `images/${uuidv4()}.${file.originalname.split('.').pop()}`,
     );
-    console.log(blob);
     // 建立 blobStream，將數據寫入到 bucket，創建了一個寫入流 writable stream，將本地文件通過流的方式上傳到 Google Cloud storage
     const blobStream = blob.createWriteStream();
     // 監聽上傳狀態，上傳完成觸發 finish 事件
-    blobStream.on('finish', () => {
+    blobStream.on('finish', async () => {
       // 設定檔案存取權限，允許讀取文件、signedUrl 於 2500/12/31 到期
       const config = {
         action: 'read',
         expires: '12-31-2500',
       };
       // 取得檔案網址
-      blob.getSignedUrl(config, (err, fileUrl) => {
-        res.send({
-          fileUrl,
+      blob.getSignedUrl(config, async (err, fileUrl) => {
+        if (err) {
+          return next(handleAppError(500, '取得檔案網址失敗'));
+        }
+        await User.findByIdAndUpdate(req.user.id, {
+          photo: fileUrl,
         });
+        handleResponse(res, 200, '大頭照更新成功', fileUrl);
       });
     });
     // 如果上傳過程中發生錯誤，觸發 error 事件
