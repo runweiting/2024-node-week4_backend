28eb68c2ef60d5e250d361793f08f613e4db0356
runweiting <whatisfuntime@gmail.com>
2024-05-24 15:34:50 +0800

    refactor: 💡 middleware/generateJWT 修正 expires

--- middlewares/generateJWT.js
+++ middlewares/generateJWT.js
@@ -8,13 +8,15 @@ const generateSendJWT = (user, statusCode, message, res) => {
   });
   user.password = undefined;
   // 將 token 到期日一併回傳，讓前台夾帶在 document.cookie
-  const expired =
-    Date.now() + parseInt(process.env.JWT_EXPIRES_DAY) * 24 * 60 * 60 * 1000;
+  // 新增 Date(當前時間戳 + 天數 * 24 * 60 * 60 * 1000(毫秒)).轉換為 UTC 格式字串
+  const expires = new Date(
+    Date.now() + parseInt(process.env.JWT_EXPIRES_DAY) * 24 * 60 * 60 * 1000,
+  ).toUTCString();
   res.status(statusCode).json({
     status: true,
     message,
     token,
-    expired,
+    expires,
   });
 };
 
@@ -24,9 +26,14 @@ const generateUrlJWT = (user, res) => {
     expiresIn: process.env.JWT_EXPIRES_DAY,
   });
   user.password = undefined;
+  // 將 token 到期日一併回傳，讓前台夾帶在 document.cookie
+  // 新增 Date(當前時間戳 + 天數 * 24 * 60 * 60 * 1000(毫秒)).轉換為 UTC 格式字串
+  const expires = new Date(
+    Date.now() + parseInt(process.env.JWT_EXPIRES_DAY) * 24 * 60 * 60 * 1000,
+  ).toUTCString();
   // 使用者透過 ‘/user/google’ 登入 google，不是透過 API，故需重新導向 res.redirect
   res.redirect(
-    `${process.env.GOOGLE_RESOURCE_OWNER_REDIRECT}?token=${token}&name=${user.name}`,
+    `${process.env.GOOGLE_RESOURCE_OWNER_REDIRECT}?token=${token}&expires=${expires}`,
   );
 };
 
