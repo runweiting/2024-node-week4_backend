e672a5f466126b6394987d35ec6c0514bbe46d6d
runweiting <whatisfuntime@gmail.com>
2024-06-18 21:42:07 +0800

    fix: 🐛 mongoose.js 移除 useNewUrlParser 和 useUnifiedTopology

--- connections/mongoose.js
+++ connections/mongoose.js
@@ -5,11 +5,6 @@ const DB = process.env.MONGODB_ATLAS_URL.replace(
 );
 
 mongoose
-  .connect(DB, {
-    // 使用新的 URL 解析器來解析 MongoDB 連接字符串
-    useNewUrlParser: true,
-    // 啟用新的 MongoDB 驅動引擎，稱為「統一拓撲設置」(Unified Topology)
-    useUnifiedTopology: true,
-  })
+  .connect(DB)
   .then((res) => console.log('資料庫連線成功'))
   .catch((err) => console.log('資料庫連接有誤', err.message));
