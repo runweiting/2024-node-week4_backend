5d81c806e963832bc77f2b4735a4a37e45bf0f0d
runweiting <whatisfuntime@gmail.com>
2024-06-19 17:59:10 +0800

    docs: ✏️ 重新生成 swagger-output

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-  'http://localhost:3010',
   'https://two024-node-week4.onrender.com',
+  'http://two024-node-week4.onrender.com',
+  'http://localhost:3010',
 ];
 
 module.exports = allowedOrigins;
