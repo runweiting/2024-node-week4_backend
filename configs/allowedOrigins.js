26cac0cb38633efafe1849693fe028c782fff2c1
runweiting <whatisfuntime@gmail.com>
2024-07-08 16:18:05 +0800

    fix: 🐛 allowedOrigins 加入前台網域

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'https://two024-node-week4.onrender.com',
   'http://two024-node-week4.onrender.com',
   'http://localhost:3010',
+  'https://runweiting.github.io',
 ];
 
 module.exports = allowedOrigins;
