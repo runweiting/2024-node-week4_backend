b932ccbe6cba4ce682d48d47c6a1533a303d8921
runweiting <whatisfuntime@gmail.com>
2024-07-08 19:49:19 +0800

    docs: ✏️ 更新 allowedOrigins

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   'http://two024-node-week4.onrender.com',
   'http://localhost:3010',
   'https://runweiting.github.io',
+  'http://localhost:5173',
 ];
 
 module.exports = allowedOrigins;
