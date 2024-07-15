0c44a0dc2df7d01ea993731e3b6c605ce1be3fa0
runweiting <whatisfuntime@gmail.com>
2024-07-15 21:52:13 +0800

    feat: 🎸 checkOrderExists

---
+++ middlewares/checkOrderExists.js
@@ -0,0 +1,44 @@
+const { handleAppError, handleErrorAsync } = require('./handleResponses');
+const Order = require('../models/ordersModel');
+
+function create_mpg_aes_decrypt(tradeInfo) {
+  try {
+    const decrypt = crypto.createDecipheriv(
+      'aes-256-cbc',
+      NEWEBPAY_HASH_KEY,
+      NEWEBPAY_HASH_IV,
+    );
+    // 關閉自動填充（padding）
+    decrypt.setAutoPadding(false);
+    const text = decrypt.update(tradeInfo, 'hex', 'utf8');
+    const plainText = text + decrypt.final('utf8');
+    const result = plainText.replace(/[\x00-\x20]+/g, '');
+    return result;
+  } catch (err) {
+    console.log('解密過程中出現錯誤:', err);
+  }
+}
+
+const decryptTradeInfo = (response) => {
+  const decryptData = JSON.parse(create_mpg_aes_decrypt(response.TradeInfo));
+  return decryptData;
+};
+
+// 共用中介軟體：檢查訂單是否存在
+const checkOrderExists = handleErrorAsync(async (req, res, next) => {
+  // 可能因測試機 response 沒有顯示 CheckCode (手冊有寫 p.23, p.50)
+  const decryptData = decryptTradeInfo(req.body);
+  const targetOrder = await Order.findOne({
+    merchantOrderNo: decryptData.Result.MerchantOrderNo,
+  }).populate({
+    path: 'user',
+    select: '_id',
+  });
+  if (!targetOrder) {
+    return handleAppError(404, '此筆訂單不存在', next);
+  }
+  req.targetOrder = targetOrder;
+  next();
+});
+
+module.exports = checkOrderExists;
--- routes/orders.js
+++ routes/orders.js
@@ -1,193 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const crypto = require('crypto');
-const Order = require('../models/ordersModel');
-const User = require('../models/usersModel');
-const {
-  handleErrorAsync,
-  handleResponse,
-  handleAppError,
-} = require('../middlewares/handleResponses');
+const OrdersController = require('../controllers/ordersController');
+const { handleErrorAsync } = require('../middlewares/handleResponses');
 const isAuth = require('../middlewares/isAuth');
 const { genNewebpayReturnUrlJWT } = require('../middlewares/generateJWT');
-const {
-  NEWEBPAY_MERCHANT_ID,
-  NEWEBPAY_HASH_KEY,
-  NEWEBPAY_HASH_IV,
-  NEWEBPAY_VERSION,
-  NEWEBPAY_RESPOND_TYPE,
-  NEWEBPAY_NOTIFY_URL,
-  NEWEBPAY_RETURN_URL,
-  NEWEBPAY_CCORE,
-  NEWEBPAY_CLIENTBACK_URL,
-} = process.env;
+const checkOrderExists = require('../middlewares/checkOrderExists');
 
 // 建立訂單
-router.post('/', isAuth, async (req, res, next) => {
-  const { userId, amt, itemDesc } = req.body;
-  const user = await User.findById(userId);
-  if (!user) {
-    return handleAppError(404, '用戶不存在', next);
-  }
-  const newOrder = await Order.create({
-    user: userId,
-    amt,
-    itemDesc,
-  });
-  handleResponse(res, 201, '新增成功', newOrder._id.toString());
-});
+router.post('/', isAuth, handleErrorAsync(OrdersController.createOrder));
 
 // 取得訂單
-router.get('/:id', isAuth, async (req, res, next) => {
-  const { id } = req.params;
-  const order = await Order.findById(id).populate({
-    path: 'user',
-    select: 'email',
-  });
-  if (!order) {
-    return handleAppError(404, '訂單不存在', next);
-  }
-  // 加密
-  const aesEncrypt = create_mpg_aes_encrypt(order);
-  const shaEncrypt = create_mpg_sha_encrypt(aesEncrypt);
-  // 回傳加密
-  const tradeInfo = {
-    MerchantID: NEWEBPAY_MERCHANT_ID.toString(),
-    TradeInfo: aesEncrypt,
-    TradeSha: shaEncrypt,
-    Version: NEWEBPAY_VERSION,
-    PayGateWay: NEWEBPAY_CCORE,
-  };
-  handleResponse(res, 200, '查詢成功', tradeInfo);
-});
+router.get('/:id', isAuth, handleErrorAsync(OrdersController.getOrder));
 
 // newebpay_return
-router.post('/newebpay_return', async (req, res, next) => {
-  // 可能因測試機 response 沒有顯示 CheckCode (手冊有寫 p.23, p.50)
-  const response = req.body;
-  console.log('response', response);
-  const data = JSON.parse(create_mpg_aes_decrypt(response.TradeInfo));
-  console.log('data', data);
-  // 驗證一、檢查資料庫是否有相同訂單
-  const targetOrder = await Order.findOne({
-    merchantOrderNo: data.Result.MerchantOrderNo,
-  }).populate({
-    path: 'user',
-    select: '_id',
-  });
-  console.log('targetOrder', targetOrder);
-  if (!targetOrder) {
-    return handleAppError(404, '此筆訂單不存在', next);
-  }
-  // 驗證二、檢查訂單是否已付款
-  if (targetOrder.isPaid) {
-    console.log(`訂單編號：${targetOrder.merchantOrderNo} 已付款。`);
-    return handleResponse(res, 200, '訂單已付款');
-  }
-  // 驗證三、比對 SHA 加密字串
-  const testShaEncrypt = create_mpg_sha_encrypt(response.TradeInfo);
-  if (response.TradeSha !== testShaEncrypt) {
-    return handleAppError(404, '付款失敗：TradeSha 不一致', next);
-  }
-  await Order.findByIdAndUpdate(targetOrder._id, {
-    isPaid: true,
-    tradeInfo: {
-      status: data.Status,
-      message: data.Message,
-      tradeNo: data.Result.TradeNo,
-      ip: data.Result.IP,
-      escrowBank: data.Result.EscrowBank,
-      paymentType: data.Result.PaymentType,
-      payTime: formattedPayTimeStr(data.Result.PayTime),
-      payerAccount5Code: data.Result.PayerAccount5Code,
-      payBankCode: data.Result.PayBankCode,
-    },
-  });
-  // 交易完成，將成功資訊儲存於資料庫
-  console.log('付款完成！訂單編號：', data.Result.MerchantOrderNo);
-  // 生成 token, expires 並導向回 UserCallback
-  genNewebpayReturnUrlJWT(targetOrder.user, res);
-});
+router.post(
+  '/newebpay_return',
+  checkOrderExists,
+  handleErrorAsync(async (req, res, next) => {
+    // 生成 token, expires 並導向回 UserCallback
+    genNewebpayReturnUrlJWT(req.targetOrder.user, res);
+  }),
+);
 
 // newebpay_notify
-router.post('/newebpay_notify', async (req, res, next) => {
-  const data = req.body;
-  // tradeInfo, tradeSha
-  console.log('notify', data);
-  res.end();
-});
-
-// 1. 將請求字串加密：AES-256-CBC 加密訂單資料
-/*
-AES (Advanced Encryption Standard) 是一種對稱加密演算法，常用於數據加密。對稱加密意指加密和解密過程使用相同的密鑰。AES 支持三種密鑰長度：128、192 和 256 位，其中 AES-256 是最強的，因為它使用 256 位的密鑰。
-CBC (Cipher Block Chaining) 是 AES 的一種區塊加密模式。
-工作原理：
-初始向量 (IV): 在 CBC 模式中，每個明文區塊在加密之前會先與上一個密文區塊進行 XOR 操作。第一個區塊沒有上一個密文區塊，所以它會與初始向量 (IV) 進行 XOR 操作。IV 是一個隨機生成的數據塊，確保相同的明文每次加密都會產生不同的密文。
-區塊加密: 每個明文區塊會與上一個密文區塊（或 IV）進行 XOR，然後通過加密算法（AES-256）進行加密。
-解密過程: 解密時，每個密文區塊會先通過解密算法解密，然後再與上一個密文區塊（或 IV）進行 XOR，以恢復原始明文。
-如此使得每個區塊的加密結果依賴於前一個區塊，增強了加密的強度。
-*/
-function create_mpg_aes_encrypt(order) {
-  const encrypt = crypto.createCipheriv(
-    'aes-256-cbc',
-    NEWEBPAY_HASH_KEY,
-    NEWEBPAY_HASH_IV,
-  );
-  const enc = encrypt.update(genDataChain(order), 'utf8', 'hex');
-  return enc + encrypt.final('hex');
-}
-
-// 2. 生成請求字串：排列參數並串聯
-function genDataChain(order) {
-  // 基本資料
-  const data = {
-    MerchantID: NEWEBPAY_MERCHANT_ID,
-    RespondType: NEWEBPAY_RESPOND_TYPE,
-    TimeStamp: Math.floor(order.timestamp / 1000).toString(),
-    Version: NEWEBPAY_VERSION,
-    MerchantOrderNo: order.merchantOrderNo,
-    Amt: order.amt,
-    ItemDesc: encodeURIComponent(order.itemDesc),
-    Email: encodeURIComponent(order.user.email),
-    NotifyURL: encodeURIComponent(NEWEBPAY_NOTIFY_URL),
-    ReturnURL: encodeURIComponent(NEWEBPAY_RETURN_URL),
-    ClientBackURL: encodeURIComponent(NEWEBPAY_CLIENTBACK_URL),
-  };
-  const dataChain = Object.entries(data)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
-  return dataChain;
-}
-
-// 3. 將 AES 加密字串產生檢查碼：SHA-256 加密驗證資料
-function create_mpg_sha_encrypt(aesEncrypt) {
-  const sha = crypto.createHash('sha256');
-  const plainText = `HashKey=${NEWEBPAY_HASH_KEY}&${aesEncrypt}&HashIV=${NEWEBPAY_HASH_IV}`;
-  return sha.update(plainText).digest('hex').toUpperCase();
-}
-
-// 4. 將 AES 加密字串進行解密
-function create_mpg_aes_decrypt(tradeInfo) {
-  try {
-    const decrypt = crypto.createDecipheriv(
-      'aes-256-cbc',
-      NEWEBPAY_HASH_KEY,
-      NEWEBPAY_HASH_IV,
-    );
-    // 關閉自動填充（padding）
-    decrypt.setAutoPadding(false);
-    const text = decrypt.update(tradeInfo, 'hex', 'utf8');
-    const plainText = text + decrypt.final('utf8');
-    const result = plainText.replace(/[\x00-\x20]+/g, '');
-    return result;
-  } catch (err) {
-    console.log('解密過程中出現錯誤:', err);
-  }
-}
-
-function formattedPayTimeStr(payTime) {
-  const formattedStr = payTime.slice(0, 10) + ' ' + payTime.slice(10);
-  return new Date(formattedStr);
-}
+router.post(
+  '/newebpay_notify',
+  checkOrderExists,
+  handleErrorAsync(OrdersController.newebpayNotify),
+);
 
 module.exports = router;
